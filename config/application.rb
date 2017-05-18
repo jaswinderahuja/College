301c1ee26bc60f8458b1859f9fa1ececd29ad55f
jaswinderahuja <jaswinderatthapar@gmail.com>
2017-05-18 20:56:58 +0000

    override view_action in application to handle error div css

--- config/application.rb
+++ config/application.rb
@@ -11,5 +11,8 @@ module College
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
+    config.action_view.field_error_proc = Proc.new { |html_tag, instance| 
+	  html_tag
+	}
   end
 end
