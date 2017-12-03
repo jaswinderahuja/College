be63216c9c45b0e1fc29648e361f695e8eddecd7
abhilashpa <abhilashpa.28@gmail.com>
2017-12-04 00:41:30 +0530

    Restructuring query constructor and adding pagination

--- app/lib/search/search_base.rb
+++ app/lib/search/search_base.rb
@@ -1,6 +1,6 @@
 module Search
 	class SearchBase
-		def initialize(query_string,sort_by=nil,city_filter=[],position=[],openings_search_fields=nil,companies_search_fields=nil)			
+		def initialize(query_string,sort_by=nil,city_filter=[],position=[],openings_search_fields=nil,companies_search_fields=nil,from=0)
 			# raise "Query string can't be blank!!" if query_string.blank?
 			@keyword = query_string
 			@sort_by = sort_by
@@ -8,18 +8,18 @@ module Search
 			@position = position
 			@openings_search_fields = openings_search_fields
 			@companies_search_fields = companies_search_fields
-			@query_constructor = QueryConstructor.new			
+			@query_constructor = QueryConstructor.new
 			@query_executor = Search::QueryExecutor.new
 		end
 
 		def search
 			if @keyword.present?
-				body = @query_constructor.openings_by_keywords(@keyword,@sort_by,@openings_search_fields,@companies_search_fields)						
-			else				
+				body = @query_constructor.openings_by_keywords(@keyword,@sort_by,@openings_search_fields,@companies_search_fields)
+			else
 				body = @query_constructor.match_all(@sort_by)
 			end
-			body = @query_constructor.location_filter(body,@city_filter) if @city_filter.present?						
-			body = @query_constructor.position_filter(body,@position) if @position.present?						
+			body = @query_constructor.location_filter(body,@city_filter) if @city_filter.present?
+			body = @query_constructor.position_filter(body,@position) if @position.present?
 			puts "#{body.inspect}"
 			openings = @query_executor.execute_opening(body)
 			response = construct_response(openings)
@@ -35,19 +35,19 @@ module Search
 			openings_hash["hits"]["hits"].each do |row|
 				company_ids << row["_parent"].to_i
 			end
-			company_ids.uniq!			
+			company_ids.uniq!
 			body = @query_constructor.filter_by_ids(company_ids)
 			companies_hash = @query_executor.execute_companies(body)
 			openings_hash["hits"]["hits"].each do |row|
 				result << {:opening_id=>row["_id"].to_s,:company=>get_company_from_hash(companies_hash,row["_parent"]),:opening=>row["_source"] }
-			end	
+			end
 			return result
-		end	
+		end
 
 		def get_company_from_hash(hash,company_id)
 			company_hash = {}
 			hash["hits"]["hits"].each do |row|
-				company_hash = row["_source"]  if row["_id"].to_s == company_id.to_s 
+				company_hash = row["_source"]  if row["_id"].to_s == company_id.to_s
 			end
 			return company_hash
 		end
