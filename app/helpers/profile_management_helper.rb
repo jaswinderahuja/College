55bf24a7c5251ed3fcfdb820a132f26a98ab2d24
jaswinder <jaswinderatthapar@gmail.com>
2017-12-30 20:57:42 +0000

    profile mgmt

--- app/helpers/profile_management_helper.rb
+++ app/helpers/profile_management_helper.rb
@@ -1,2 +1,13 @@
 module ProfileManagementHelper
+	def campus_info
+		campus = CampusUser.where(:user_id=>current_user.id).first.campus
+		address = campus.campus_address
+		return [campus,address]
+	end
+
+	def city_state(pincode)
+		rows = ActiveRecord::Base.connection.select_rows("select c.name,s.name from pincodes p 
+			inner join cities c on p.city_id=c.id 
+			inner join states s on c.state_id=s.id where p.pincode=#{pincode}").first
+	end
 end
