1ccc72b0310a28df36f9d0dff6ba085168712b37
jaswinder <jaswinderatthapar@gmail.com>
2017-12-31 08:38:57 +0000

    profile mgmt UI

--- public/javascripts/campus_connect/college/profile_management.js
+++ public/javascripts/campus_connect/college/profile_management.js
@@ -1,7 +1,40 @@
 var CampusConnect = CampusConnect || {};
 CampusConnect.College = CampusConnect.College || {};
 CampusConnect.College.ProfileManagement = function() {
-        
+
+
+	var campus_info_table_init = function(){
+		$('#edit_campus_info').click(function(){
+		    $('#edit_campus_info').hide();
+		    $('#campus_info_table label').each(function(){
+		        $(this).hide();     
+		    });  
+		    $('#campus_info_table .ui.input').each(function(){
+		        $(this).show();     
+		    });  
+		      $("#campus_info_table tfoot").show() // enabling save button
+		  });
+
+		  $('#cancel_edit_campus_info').click(function(){    
+		      $("#campus_info_table tfoot").hide() // disabling save button
+		      $(".ui.error.message.campus_info").html('');
+		    $('#campus_info_table label').each(function(){
+		        $(this).show();     
+		    });  
+		      $('#campus_info_table .ui.input').each(function(){
+		        $(this).hide();
+		      });  
+		    $('#edit_campus_info').show();
+		  });
+	}
+
+	var ready = function(){
+		$(document).ready(function(){
+			campus_info_table_init();	
+		  	CampusConnect.College.Validation.campusInfo();
+		})
+	}();
+
     return {
         
     };
