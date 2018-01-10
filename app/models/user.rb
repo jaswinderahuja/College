53f13a86da96d91374a6d8c00fbec14dabf1f9a1
jaswinder <jaswinderatthapar@gmail.com>
2018-01-10 17:02:10 +0000

    user validate in model

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,6 +7,7 @@ class User < ApplicationRecord
   has_many :campus_users
   has_many :invitations
   has_many :user_tickets
+  validates :email, presence: true
   
   def update_user(options)
   		self.firstname = options[:firstname]
