0f1e42995d37902f02e226e2603c469d28594872
jaswinder <jaswinderatthapar@gmail.com>
2018-01-21 16:41:13 +0530

    forgot password with devise

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -27,7 +27,19 @@ Rails.application.configure do
   end
 
   # Don't care if the mailer can't send.
-  config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.default_url_options = {host: "localhost", port: 3000}
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    :address        => 'smtp.gmail.com',
+    :domain         => 'example.com',
+    :port           => 587,
+    :user_name      => 'campus.connect.corp@gmail.com',
+    :password       => '8KrC1vzB#',
+    :authentication => :plain,
+    :enable_starttls_auto => true
+  }
+  config.action_mailer.perform_deliveries = true
 
   config.action_mailer.perform_caching = false
 
