fb1f7da37c7f53e5ecb7433ede23f8336bddd550
jaswinder <jaswinderatthapar@gmail.com>
2018-01-22 00:10:48 +0530

    forgot password edit UI

--- app/controllers/users/passwords_controller.rb
+++ app/controllers/users/passwords_controller.rb
@@ -7,9 +7,12 @@ class Users::PasswordsController < Devise::PasswordsController
   end
 
   # POST /resource/password
-  # def create
-  #   super
-  # end
+  def create
+    puts "========="
+    p params
+    puts "========="
+    super
+  end
 
   # GET /resource/password/edit?reset_password_token=abcdef
   # def edit
