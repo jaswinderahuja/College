38ec314764f3e048e91cd6c610b0cf5ab46f8550
jaswinder <jaswinderatthapar@gmail.com>
2018-01-21 23:03:08 +0530

    forgot password UI changes

--- app/controllers/users/passwords_controller.rb
+++ app/controllers/users/passwords_controller.rb
@@ -1,4 +1,6 @@
 class Users::PasswordsController < Devise::PasswordsController
+  layout 'users'
+
   # GET /resource/password/new
   # def new
   #   super
@@ -19,14 +21,14 @@ class Users::PasswordsController < Devise::PasswordsController
   #   super
   # end
 
-  # protected
+  protected
 
-  # def after_resetting_password_path_for(resource)
-  #   super(resource)
-  # end
+  def after_resetting_password_path_for(resource)
+    super(resource)
+  end
 
   # The path used after sending reset password instructions
-  # def after_sending_reset_password_instructions_path_for(resource_name)
-  #   super(resource_name)
-  # end
+  def after_sending_reset_password_instructions_path_for(resource_name)
+    super(resource_name)
+  end
 end
